1ad429f8119643f62eb103a050748d9821fe10b4
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2026-01-25 21:13:52 +0000

    added answer code

--- code.js
+++ code.js
@@ -1,13 +1,27 @@
-function randint(min,max) {
+function randint(min,max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 	
-function displayNum() {
+function displayNum(){
 	let a, b;
 	a = randint(1,98); b = randint(a+1,99);
 	document.getElementById("question").innerText = a + "/" + b + " =";
-
 }
 
-
+function check(num) {
+	let a, b, c, txt, ind, ans, nextDig;
+	txt = document.getElementById("question").innerText;
+	ind = txt.indexOf("/");
+	a = txt.slice(0,ind); b = txt.slice(ind+1,-2); c = (a/b).toString();
+	ans = document.getElementById("answer").innerText.slice(2,-1);
+	if (ans.length + 2 == c.length) {
+		return;		//win
+	}
+	nextDig = c[2+ans.length];
+	if (num == nextDig) {
+		document.getElementById("answer").innerText = c.slice(0,3+ans.length)+"_";		//correct
+	} else {
+		return;		//incorrect
+	}
+}
