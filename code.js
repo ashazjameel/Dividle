2aacd124e41b98b2c3e3758040583c9b8dd3e558
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2026-01-25 20:04:04 +0000

    formatting

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
-function randint(min,max){
+function randint(min,max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 	
-function displayNum(){
-	let a,b;
+function displayNum() {
+	let a, b;
 	a = randint(1,98); b = randint(a+1,99);
 	document.getElementById("question").innerText = a + "/" + b + "=";
+
 }
