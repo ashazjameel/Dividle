80af7160e5cc0d09a8e0c01e27e451b6c54c6795
ashazjameel <161173725+ashazjameel@users.noreply.github.com>
2026-01-26 01:40:10 +0000

    finally added resets

--- code.js
+++ code.js
@@ -14,6 +14,7 @@ function displayNum() {
 function correct(c){
 	document.getElementById("answer").innerText = c;
 	document.getElementById("answer").style.color = "green";
+	window.setTimeout(displayNum,750);
 }
 
 function check(num) {
@@ -39,6 +40,7 @@ function check(num) {
 		return;		//incorrect
 	}
 }
+
 
 
 
